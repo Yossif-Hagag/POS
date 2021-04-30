7ef078ff765cd940c58e74dfac3ac95e0a184bf9
Yossif Hagag <yousefhagag179@gmail.com>
2021-04-30 05:39:34 +0200

    First Commit

---
+++ src/pos/POS.java
@@ -0,0 +1,21 @@
+package pos;
+
+import GUI.*;
+
+import javafx.application.Application;
+
+
+
+
+
+public class POS  {
+
+
+
+    public static void main(String[] args) {
+      
+         Application.launch(LoginFrame.class, args);
+    }
+
+}
+
